83704d9fbbf9def1543e4e55dca3ec34dd020c5d
Danning Chen <danning.cc@gmail.com>
2020-09-13 15:14:52 +0200

    add in-app review

--- app/src/main/java/com/danshima/animor/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/danshima/animor/ui/notifications/NotificationsFragment.kt
@@ -1,6 +1,7 @@
 package com.danshima.animor.ui.notifications
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.danshima.animor.R
+import com.google.android.play.core.review.ReviewManagerFactory
 
 class NotificationsFragment : Fragment() {
 
@@ -27,5 +29,28 @@ class NotificationsFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        inAppReview()
+    }
+
+    fun inAppReview() {
+        val reviewManager = ReviewManagerFactory.create(requireActivity())
+        val requestReviewFlow = reviewManager.requestReviewFlow()
+        requestReviewFlow.addOnCompleteListener { request ->
+            if (request.isSuccessful) {
+                val reviewInfo = request.result
+                val flow = reviewManager.launchReviewFlow(requireActivity(), reviewInfo)
+                flow.addOnCompleteListener {
+                    // The flow has finished. The API does not indicate whether the user
+                    // reviewed or not, or even whether the review dialog was shown. Thus, no
+                    // matter the result, we continue our app flow.
+                }
+            } else {
+                Log.d("Error: ", request.exception.toString())
+            }
+        }
     }
 }
